cdf05c0c5b2f9824945a868dd5f78d258675eaf2
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-01-17 05:56:23 +0400

    Add ProductInformationFragment via FragmentManager

--- app/src/main/java/tk/foodpedia/android/MainActivity.java
+++ app/src/main/java/tk/foodpedia/android/MainActivity.java
@@ -2,6 +2,7 @@ package tk.foodpedia.android;
 
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,8 +12,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.findFragmentById(R.id.fragment_container) == null) {
+            fm.beginTransaction().add(R.id.fragment_container, ProductInformationFragment.newInstance()).commit();
+        }
+
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/tk/foodpedia/android/ProductInformationFragment.java
+++ app/src/main/java/tk/foodpedia/android/ProductInformationFragment.java
@@ -6,10 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainActivityFragment extends Fragment {
+public class ProductInformationFragment extends Fragment {
+
+    public static ProductInformationFragment newInstance() {
+        return new ProductInformationFragment();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_product_information, container, false);
     }
 }
