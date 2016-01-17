a9e775f65ef75332095f34ed550a531849e4d60f
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-01-17 04:58:06 +0400

    Initial commit

---
+++ app/src/main/java/tk/foodpedia/android/MainActivity.java
@@ -0,0 +1,23 @@
+package tk.foodpedia.android;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "<TBD>", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
