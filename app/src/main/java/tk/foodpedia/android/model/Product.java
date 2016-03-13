d361b0940e36592c067e2cf834602ebf0cab417d
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-03-13 22:29:34 +0400

    Update layout of Product fragment, set up typography

--- app/src/main/java/tk/foodpedia/android/model/Product.java
+++ app/src/main/java/tk/foodpedia/android/model/Product.java
@@ -1,7 +1,10 @@
 package tk.foodpedia.android.model;
 
+import tk.foodpedia.android.R;
 import tk.foodpedia.android.model.meta.DoubleValue;
 import tk.foodpedia.android.model.meta.StringValue;
+
+import static tk.foodpedia.android.util.StringHelper.format;
 
 public class Product extends Downloadable {
     private StringValue ean;
@@ -42,12 +45,20 @@ public class Product extends Downloadable {
         return mass == null ? null : mass.getValue();
     }
 
+    public String getMassFormatted() {
+        return format(R.string.format_mass, getMass() == null ? "?" : getMass());
+    }
+
     public void setMass(StringValue mass) {
         this.mass = mass;
     }
 
     public Double getEnergy() {
         return energy == null ? null : energy.getValue();
+    }
+
+    public String getEnergyFormatted() {
+        return format(R.string.format_energy, getEnergy() == null ? "?" : getEnergy());
     }
 
     public void setEnergy(DoubleValue energy) {
@@ -58,12 +69,20 @@ public class Product extends Downloadable {
         return proteins == null ? null : proteins.getValue();
     }
 
+    public String getProteinsFormatted() {
+        return format(R.string.format_proteins, getProteins() == null ? "?" : getProteins());
+    }
+
     public void setProteins(DoubleValue proteins) {
         this.proteins = proteins;
     }
 
     public Double getFat() {
         return fat == null ? null : fat.getValue();
+    }
+
+    public String getFatFormatted() {
+        return format(R.string.format_fat, getFat() == null ? "?" : getFat());
     }
 
     public void setFat(DoubleValue fat) {
@@ -74,12 +93,20 @@ public class Product extends Downloadable {
         return carbohydrates == null ? null : carbohydrates.getValue();
     }
 
+    public String getCarbohydratesFormatted() {
+        return format(R.string.format_carbohydrates, getCarbohydrates() == null ? "?" : getCarbohydrates());
+    }
+
     public void setCarbohydrates(DoubleValue carbohydrates) {
         this.carbohydrates = carbohydrates;
     }
 
     public String getIngredients() {
         return ingredients == null ? null : ingredients.getValue();
+    }
+
+    public String getIngredientsFormatted() {
+        return getIngredients() == null ? "" : format(R.string.format_ingredients, getIngredients());
     }
 
     public void setIngredients(StringValue ingredients) {
--- app/src/main/java/tk/foodpedia/android/util/StringHelper.java
+++ app/src/main/java/tk/foodpedia/android/util/StringHelper.java
@@ -1,7 +1,11 @@
 package tk.foodpedia.android.util;
 
+import android.support.annotation.StringRes;
+
 import java.io.InputStream;
 import java.util.Scanner;
+
+import tk.foodpedia.android.App;
 
 public final class StringHelper {
     private StringHelper() { /* */ }
@@ -9,5 +13,9 @@ public final class StringHelper {
     public static String fromStream(InputStream is) {
         Scanner scanner = new Scanner(is).useDelimiter("\\A");
         return scanner.hasNext() ? scanner.next() : "";
+    }
+
+    public static String format(@StringRes int strId, Object... formatArgs) {
+        return App.getContext().getString(strId, formatArgs);
     }
 }
