dfe4163a7e0e48bcb0b13471d2322620c8dbb3d7
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-02 17:09:40 +0400

    Implement history via RecyclerView

--- app/src/main/java/tk/foodpedia/android/fragment/HistoryFragment.java
+++ app/src/main/java/tk/foodpedia/android/fragment/HistoryFragment.java
@@ -1,13 +1,20 @@
 package tk.foodpedia.android.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import tk.foodpedia.android.R;
 import tk.foodpedia.android.concurrent.Database;
@@ -15,6 +22,11 @@ import tk.foodpedia.android.concurrent.Database.DatabaseCallbacks;
 import tk.foodpedia.android.db.ProductRecord;
 
 public class HistoryFragment extends BaseFragment implements DatabaseCallbacks<ArrayList<ProductRecord>> {
+    private static final DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.US);
+
+    private RecyclerView historyRecyclerView;
+    private ProgressBar historyProgressBar;
+    private HistoryAdapter historyAdapter;
 
     public static HistoryFragment newInstance() {
         return new HistoryFragment();
@@ -24,20 +36,69 @@ public class HistoryFragment extends BaseFragment implements DatabaseCallbacks<A
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setTitle(R.string.label_history);
+
         Database.getInstance().loadHistory(this);
-        return inflater.inflate(R.layout.fragment_history, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        historyRecyclerView = (RecyclerView) rootView.findViewById(R.id.history_recycler_view);
+        historyRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        historyRecyclerView.setAdapter(historyAdapter = new HistoryAdapter());
+
+        historyProgressBar = (ProgressBar) rootView.findViewById(R.id.history_progress_bar);
+
+        return rootView;
     }
 
 
-    // TODO: rewrite
     @Override
     public void onReadDatabaseCompleted(ArrayList<ProductRecord> productRecords) {
         if (!isAdded()) return;
-        for (ProductRecord productRecord : productRecords) {
-            View record = getActivity().getLayoutInflater().inflate(R.layout.view_product_record, null);
-            ((TextView) record.findViewById(R.id.product_record_name)).setText(productRecord.name);
-            ((TextView) record.findViewById(R.id.product_record_last_viewed)).setText(productRecord.lastViewed.toString());
-            ((LinearLayout) getView()).addView(record);
+        historyAdapter.setProductRecords(productRecords);
+        historyRecyclerView.setVisibility(View.VISIBLE);
+        historyProgressBar.setVisibility(View.GONE);
+    }
+
+
+    private class HistoryAdapter extends Adapter<ProductRecordViewHolder> {
+        private ArrayList<ProductRecord> productRecords = new ArrayList<>();
+
+        public void setProductRecords(ArrayList<ProductRecord> productRecords) {
+            this.productRecords = productRecords;
+            notifyDataSetChanged();
+        }
+
+
+        @Override
+        public ProductRecordViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+            View record = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_product_record, parent, false);
+            return new ProductRecordViewHolder(record);
+        }
+
+
+        @Override
+        public void onBindViewHolder(ProductRecordViewHolder holder, int i) {
+            ProductRecord productRecord = productRecords.get(i);
+            holder.name.setText(productRecord.name);
+            holder.lastViewed.setText(dateFormat.format(productRecord.lastViewed));
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return productRecords.size();
+        }
+    }
+
+
+    private class ProductRecordViewHolder extends ViewHolder {
+        final TextView name;
+        final TextView lastViewed;
+
+        public ProductRecordViewHolder(View productRecordView) {
+            super(productRecordView);
+            name = (TextView) productRecordView.findViewById(R.id.product_record_name);
+            lastViewed = (TextView) productRecordView.findViewById(R.id.product_record_last_viewed);
         }
     }
 }
